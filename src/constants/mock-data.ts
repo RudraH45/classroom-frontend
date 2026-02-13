5bc38323c6dd394162d280dd0daff3f4866b83f8
RudraH45 <rudra89howale@gmail.com>
2026-02-14 02:19:32 +0530

    created mock backend layer with refine dataProvider

---
+++ src/constants/mock-data.ts
@@ -0,0 +1,25 @@
+import { Subject } from "../types";
+
+export const mockSubjects: Subject[] = [
+  {
+    id: 1,
+    code: "CS101",
+    name: "Introduction to Computer Science",
+    department: "Computer Science",
+    description: "Foundational concepts in computing, programming, and problem-solving.",
+  },
+  {
+    id: 2,
+    code: "MATH201",
+    name: "Calculus II",
+    department: "Mathematics",
+    description: "Techniques of integration, sequences and series, and parametric equations.",
+  },
+  {
+    id: 3,
+    code: "HIST150",
+    name: "World History: Early Civilizations",
+    department: "History",
+    description: "Survey of major early civilizations and their cultural and political developments.",
+  },
+];
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,25 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import { BaseRecord, DataProvider, GetListParams, GetListResponse } from "@refinedev/core";
+import { mockSubjects } from "@/constants/mock-data.ts";
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>( {resource}:
+    GetListParams): Promise<GetListResponse<TData>> => {       //It returns a list of records for a specific(the one we choose) resource.
+      if (resource !== 'subjects') return {          //If Refine asks for anything other than "subjects
+         // Return empty data
+        data: [] as TData[],
+        total: 0
+      };
+
+      return {
+        data: mockSubjects as unknown as TData[],
+        total: mockSubjects.length
+      }
+    },
+
+    getOne: async () => {throw new Error("Method not implemented.")},
+    create: async () => {throw new Error("Method not implemented.")},
+    update: async () => {throw new Error("Method not implemented.")},
+    deleteOne: async () => {throw new Error("Method not implemented.")},
+
+    getApiUrl: () => '',
+}
